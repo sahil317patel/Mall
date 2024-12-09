843bedc447018c484fe419b93d5e580f916cf188
sahil317patel <sahil317patel@gmail.com>
2024-12-09 22:56:02 +0530

    mall doctypes updated

---
+++ mall/mall_management/doctype/floor_master/test_floor_master.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2024, sahlil and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestFloorMaster(FrappeTestCase):
+	pass
---
+++ mall/mall_management/doctype/mall_master/mall_master.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2024, sahlil and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Mall Master", {
+// 	refresh(frm) {
+
+// 	},
+// });
