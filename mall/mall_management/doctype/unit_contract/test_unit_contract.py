440bd8d4f90f55726ca972cbb602a5b67b9ae245
sahil317patel <sahil317patel@gmail.com>
2024-12-10 21:11:46 +0530

    latest changes updated

---
+++ mall/mall_management/doctype/unit_contract/test_unit_contract.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2024, sahlil and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestUnitContract(FrappeTestCase):
+	pass
